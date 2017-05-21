7ec2cc7b9da8ef54b8d81170957718fd30badb7b
Dmitriy zotov <Dmitriyzotovtest@yandex.ru>
2017-05-21 23:30:28 +0300

    second commit idea

---
+++ src/main/java/gitoverview/gittest.java
@@ -0,0 +1,21 @@
+package gitoverview;
+
+/**
+ * Created by USERrr on 21.05.2017.
+ */
+public class gittest {
+    long g;
+
+    public long getG() {
+        return g;
+    }
+
+    public void setG(long g) {
+        this.g = g;
+    }
+
+    public gittest(long g) {
+
+        this.g = g;
+    }
+}
